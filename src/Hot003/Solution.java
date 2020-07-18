72919af8fba4736f6d282afc4c973ffd841b82b9
Lemon <3117004781@mail2.gdut.edu.cn>
2020-07-18 13:59:47 +0800

    // 第三题

---
+++ src/Hot003/Solution.java
@@ -0,0 +1,20 @@
+package Hot003;
+
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        int ans = 0;
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(int left = 0,right = 0; right<length;right++){
+            char temp = s.charAt(right);
+            if(hashMap.containsKey(temp)){ //重复
+                left = Math.max(hashMap.get(temp),left);
+            }
+            ans = Math.max(ans,right-left+1);
+            hashMap.put(temp,right+1);
+        }
+        return ans;
+    }
+}
